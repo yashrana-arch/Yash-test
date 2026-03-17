5f76aa01fdc0fa8359f9e6ac41367f134bb8e7cf
Yash Fitpass <yash.rana@fitpass.co.in>
2026-03-17 15:14:16 +0530

    Ecomerce

--- main.java
+++ main.java
@@ -5,20 +5,24 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
-public class main {
-    public static void main(String[] args) throws IOException {
-
+public class Main {
+    public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/", new MyHandler());
         server.setExecutor(null);
         server.start();
 
-        System.out.println("Server started at http://localhost:8000");
+        System.out.println("Server running at http://localhost:8000");
     }
 
     static class MyHandler implements HttpHandler {
         public void handle(HttpExchange t) throws IOException {
-            String response = "<h1>Hello, this is my Java website! And i am yash </h1>";
+            String response = new String(
+                java.nio.file.Files.readAllBytes(
+                    java.nio.file.Paths.get("index.html")
+                )
+            );
+
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
